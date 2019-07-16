1666910ad743197cf2eab4e383baa1b7ff278de0
xingtan <45331711+xingtan@users.noreply.github.com>
2019-07-16 18:55:50 +0800

    Create baidu.js

---
+++ js/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?ff8d590d985f88e51b01fc641d6005d1";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
